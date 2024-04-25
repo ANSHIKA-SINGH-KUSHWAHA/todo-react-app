60f32c83c729a713eab2021ae6a96dab908726f1
ANSHIKA-SINGH-KUSHWAHA <160586467+ANSHIKA-SINGH-KUSHWAHA@users.noreply.github.com>
2024-04-25 19:59:22 +0530

    4function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+ import logo from './logo.svg';
+ import './App.css';
+ import { useState } from 'react';
 
-function App() {
-  return (
-    <div>
-      Hello todo
-    </div>
-  );
-}
+ var count =0
+ function App()
+ {
+   const [todoList, setTodoList] = useState ([
+    {
+      id: count++,
+      todo: "Need to complete Homework",
+      completed: true
+    },
+    {
+      id: count++,
+      todo: "Need to buy groceries",
+      completed: false
+    },
+    {
+      id: count++,
+      todo: "Need to fix vehical",
+      completed: false
+    },
+    {
+      id: count++,
+      todo: "Complete project",
+      completed: false
+    },
+  ])
 
-export default App;
+    
+  const addTodo = () =>
+   {
+    //  console.log("Add todo")
+    //   console.log("Text readed:" , document.getElementById("input").value);
+     
+     //console.log("Add todo")
+     //console.log("Text readed:",documentgetElementById("input").value)
+    // console.log("todoList",todolist)
+    //  todolist.push(document.getElementById("input").value)
+    // setTodoList(todolist)
+    //  console.log("todoList: ", todolist);
+    const text = document.getElementById("input").value
+    const todoObject ={
+      id: count++,
+      todo: text,
+      completed: false
+     }
+     setTodoList([...todoList, todoObject])
+   }
+
+   const deleteTodo = (id) =>
+   {
+    console.log("deletTodo", id);
+     var tempTodoList= todoList.filter(iterator =>
+      {
+        return id != iterator.id
+
+        // if(id===iterator.id)
+        // {
+        //   return false
+        // }
+        // else
+        // {
+        //   return true
+        // }
+       
+      })
+      setTodoList([...tempTodoList])
+    
+   }
+
+   const completeTodo = (id) =>
+   {
+    console.log("completed:", id)
+    
+    var tempTodoList= todoList.map(iterator =>
+      {
+        if(id === iterator.id)
+        {
+          iterator.completed = !iterator.completed
+          return iterator
+        }
+        else
+        {
+          return iterator
+        }
+      })
+      setTodoList([...tempTodoList])    
+  }
+
+  
+   return (
+     <div>
+        <h1>Hello Todo Appliation</h1>
+        <h4>(By Anshika Singh Kushwaha)</h4>
+
+        <input type ='text' id='input' placeholder ='Enter todo here...'/>
+       <button onClick={addTodo}>Add Todo</button>
+
+       <div>
+        <ul>
+           {todoList.map(iterator =>
+             {
+              
+               return <li key={iterator.id}>
+                { iterator.completed ==true ?
+
+                <>
+                  <input type='checkbox'onChange={()=>completeTodo(iterator.id)} checked/>
+                  <s>{iterator.todo}</s>
+                </>:
+                <>
+                  <input type='checkbox' onChange={()=>completeTodo(iterator.id)}/>
+                  {iterator.todo}
+                </>
+                }
+                <button onClick={()=>deleteTodo(iterator.id)}>Delete</button>
+                </li>
+            })
+             }
+    
+         </ul>
+        </div>
+     </div>
+   );
+ }
+
+ export default App;
